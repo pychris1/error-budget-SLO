7422f314a3185db7cd21a4d22cf2c8e11dd55339
James Robertson <140358457+pychris1@users.noreply.github.com>
2026-05-12 15:31:27 -0400

    Add HTTP request simulator with Prometheus metrics
    Implement a simple HTTP request simulator with Prometheus metrics.

---
+++ app.py
@@ -0,0 +1,24 @@
+import random
+import time
+from prometheus_client import start_http_server, Counter
+
+# Define our SLI metrics
+REQUESTS = Counter('http_requests_total', 'Total HTTP Requests')
+FAILURES = Counter('http_requests_failed_total', 'Total Failed HTTP Requests')
+
+if __name__ == '__main__':
+    # Start the prometheus metrics server on port 8000
+    start_http_server(8000)
+    print("App is running... sending metrics to :8000/metrics")
+    
+    while True:
+        REQUESTS.inc() # Increment total requests
+      
+        # Simulate a 20% failure rate
+        if random.random() < 0.20:
+            FAILURES.inc()
+            print("Request Failed!")
+        else:
+            print("Request Succeeded.")
+            
+        time.sleep(1) # Wait 1 second before next request
